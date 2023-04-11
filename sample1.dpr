da95d6b9e572cac537545aa4e61e154ee6387f2f
JanAukToy <fxhnx731@ybb.ne.jp>
2023-04-11 23:36:36 +0900

    none

---
+++ sample1.dpr
@@ -0,0 +1,14 @@
+program sample1;
+
+uses
+  Vcl.Forms,
+  Form_Main in 'Form_Main.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
