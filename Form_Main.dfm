d9b74137c9390d97a5dd753fe3d847d87e9a6819
JanAukToy <fxhnx731@ybb.ne.jp>
2023-04-21 20:52:52 +0900

    arrangement components

--- Form_Main.dfm
+++ Form_Main.dfm
@@ -5,11 +5,14 @@ object FormMain: TFormMain
   ClientHeight = 461
   ClientWidth = 734
   Color = clBtnFace
+  Constraints.MinHeight = 500
+  Constraints.MinWidth = 500
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -12
   Font.Name = 'Meiryo UI'
   Font.Style = []
+  Menu = MainMenu1
   OldCreateOrder = False
   Position = poScreenCenter
   OnCreate = FormCreate
@@ -25,6 +28,26 @@ object FormMain: TFormMain
     DoubleBuffered = True
     ParentDoubleBuffered = False
     TabOrder = 0
-    ExplicitHeight = 478
+    ExplicitLeft = 8
+    ExplicitTop = 20
+    ExplicitHeight = 441
+  end
+  object MainMenu1: TMainMenu
+    Left = 360
+    Top = 240
+    object mi_Settings: TMenuItem
+      Caption = 'Settings'
+      object mi_LanguageSettings: TMenuItem
+        Caption = 'Language'
+        object mi_Lang_English: TMenuItem
+          Caption = 'English'
+          OnClick = mi_Lang_EnglishClick
+        end
+        object mi_Lang_Japanese: TMenuItem
+          Caption = 'Japanese'
+          OnClick = mi_Lang_JapaneseClick
+        end
+      end
+    end
   end
 end
--- cmp_DevicePage.pas
+++ cmp_DevicePage.pas
@@ -7,33 +7,96 @@ unit cmp_DevicePage;
 interface
 
 uses
-  System.SysUtils, System.Classes, Vcl.Controls, Vcl.StdCtrls, Vcl.ComCtrls;
+  System.SysUtils, System.Classes, Vcl.Controls, Vcl.StdCtrls, Vcl.ComCtrls,
+
+  cls_AudioDevice;
 
 type
-  TDevicePage = class(TTabSheet)
-    procedure OnResizeDevicePage(Sender: TObject);
-  private
-    f_DevicePropertiesBox: TGroupBox;
-    f_AudioPropertiesBox: TGroupBox;
+  TLanguageType = (ltEnglish, ltJapanese);
 
-    procedure CreateDevicePropertiesBox;
-    procedure CreateAudioPropertiesBox;
+  TPropertyBox = class(TGroupBox)
+  private
   public
-    constructor Create(AOwner: TComponent); override;
+    constructor Create(AOwner: TComponent);
     destructor Destroy; override;
   end;
 
+  TDevicePropertyBox = class(TPropertyBox)
+  private
+    f_Device: TAudioDevice;
+
+    procedure SetLanguage(const a_LanguageType: TLanguageType);
+  public
+    constructor Create(AOwner: TComponent; const a_Device: TAudioDevice;
+      const a_LanguageType: TLanguageType);
+    destructor Destroy; override;
+    property Language: TLanguageType write SetLanguage;
+
+    procedure UpdateProperties();
+  end;
+
+  TAdudioPropertyBox = class(TPropertyBox)
+  private
+    f_Device: TAudioDevice;
+
+    procedure SetLanguage(const a_LanguageType: TLanguageType);
+  public
+    constructor Create(AOwner: TComponent; const a_Device: TAudioDevice;
+      const a_LanguageType: TLanguageType);
+    destructor Destroy; override;
+    property Language: TLanguageType write SetLanguage;
+
+    procedure UpdateProperties();
+  end;
+
+  TDevicePage = class(TTabSheet)
+    procedure OnResizeDevicePage(Sender: TObject);
+  private
+    f_Device: TAudioDevice;
+    f_DevicePropertyBox: TDevicePropertyBox;
+    f_AudioPropertyBox: TAdudioPropertyBox;
+
+    procedure SetLanguage(const a_LanguageType: TLanguageType);
+  public
+    constructor Create(AOwner: TComponent; const a_Device: TAudioDevice;
+      const a_LanguageType: TLanguageType);
+    destructor Destroy; override;
+
+    property Language: TLanguageType write SetLanguage;
+  end;
+
+const
+  PITCH_SIZE: Integer = 10;
+
 implementation
+
+uses
+  System.StrUtils;
+
+{ TDevicePage }
 
 // *****************************************************************************
 // Constructor
-constructor TDevicePage.Create(AOwner: TComponent);
+constructor TDevicePage.Create(AOwner: TComponent; const a_Device: TAudioDevice;
+  const a_LanguageType: TLanguageType);
 begin
   inherited Create(AOwner);
 
+  // Store Argments
+  f_Device := a_Device;
+
+  // Set Caption
+  Self.Caption := f_Device.FriendlyName;
+
   // Create Child Boxes
-  CreateDevicePropertiesBox();
-  CreateAudioPropertiesBox();
+  f_DevicePropertyBox := TDevicePropertyBox.Create(Self, a_Device,
+    a_LanguageType);
+  f_AudioPropertyBox := TAdudioPropertyBox.Create(Self, a_Device,
+    a_LanguageType);
+
+  // Set Parent
+  f_DevicePropertyBox.Parent := Self;
+  f_AudioPropertyBox.Parent := Self;
 
   // Set Resize Event
   Self.OnResize := OnResizeDevicePage;
@@ -43,65 +106,131 @@ end;
 // Destructor
 destructor TDevicePage.Destroy;
 begin
-  if Assigned(f_DevicePropertiesBox) then
-    FreeAndNil(f_DevicePropertiesBox);
-  if Assigned(f_AudioPropertiesBox) then
-    FreeAndNil(f_AudioPropertiesBox);
+  if Assigned(f_DevicePropertyBox) then
+    FreeAndNil(f_DevicePropertyBox);
+  if Assigned(f_AudioPropertyBox) then
+    FreeAndNil(f_AudioPropertyBox);
 
   inherited;
 end;
 
 // *****************************************************************************
-// Create Audio Properties GroupBox
-procedure TDevicePage.CreateAudioPropertiesBox;
-begin
-  // Create
-  f_DevicePropertiesBox := TGroupBox.Create(nil);
-
-  // Settings
-  f_DevicePropertiesBox.Align := alLeft;
-  f_DevicePropertiesBox.AlignWithMargins := True;
-  f_DevicePropertiesBox.Margins.SetBounds(10, 10, 10, 10);
-  f_DevicePropertiesBox.Padding.SetBounds(10, 10, 10, 10);
-  f_DevicePropertiesBox.Caption := 'Device Properties';
-
-  // Assign Parent
-  f_DevicePropertiesBox.Parent := Self;
-end;
-
-// *****************************************************************************
-// Create Device Properties GroupBox
-procedure TDevicePage.CreateDevicePropertiesBox;
-begin
-  // Create
-  f_AudioPropertiesBox := TGroupBox.Create(nil);
-
-  // Settings
-  f_AudioPropertiesBox.Align := alRight;
-  f_AudioPropertiesBox.AlignWithMargins := True;
-  f_AudioPropertiesBox.Margins.SetBounds(10, 10, 10, 10);
-  f_AudioPropertiesBox.Padding.SetBounds(10, 10, 10, 10);
-  f_AudioPropertiesBox.Caption := 'Audio Properties';
-
-  // Assign Parent
-  f_AudioPropertiesBox.Parent := Self;
-end;
-
-// *****************************************************************************
 // Device Page Resize Event
 procedure TDevicePage.OnResizeDevicePage(Sender: TObject);
+var
+  l_Width: Integer;
 begin
   // Resizing Child GroupBox
 
-  if Assigned(f_DevicePropertiesBox) then
-  begin
-    f_DevicePropertiesBox.Width := Self.Width div 2 - 15;
-  end;
+  // Set Width to Propety Box
+  l_Width := Self.Width div 2 - Round(PITCH_SIZE * 2);
+  f_DevicePropertyBox.Width := l_Width;
+  f_AudioPropertyBox.Width := l_Width;
+end;
 
-  if Assigned(f_AudioPropertiesBox) then
-  begin
-    f_AudioPropertiesBox.Width := Self.Width div 2 - 15;
-  end;
+// *****************************************************************************
+// Set Language
+procedure TDevicePage.SetLanguage(const a_LanguageType: TLanguageType);
+begin
+  // Set Language at PropertyBoxes
+  f_DevicePropertyBox.Language := a_LanguageType;
+  f_AudioPropertyBox.Language := a_LanguageType;
+end;
+
+{ TAdudioPropertyBox }
+
+// *****************************************************************************
+// Constructor
+constructor TAdudioPropertyBox.Create(AOwner: TComponent;
+  const a_Device: TAudioDevice; const a_LanguageType: TLanguageType);
+begin
+  inherited Create(AOwner);
+
+  // Store Argments
+  f_Device := a_Device;
+
+  // Set Language
+  SetLanguage(a_LanguageType);
+end;
+
+// *****************************************************************************
+// Destructor
+destructor TAdudioPropertyBox.Destroy;
+begin
+  inherited;
+end;
+
+// *****************************************************************************
+// Set LanguageType
+procedure TAdudioPropertyBox.SetLanguage(const a_LanguageType: TLanguageType);
+begin
+  Self.Caption := IfThen(a_LanguageType = ltEnglish, 'Audio Properties',
+    'オーディオ設定');
+end;
+
+// *****************************************************************************
+// Update Props
+procedure TAdudioPropertyBox.UpdateProperties;
+begin;
+end;
+
+{ TDevicePropertyBox }
+
+// *****************************************************************************
+// Constructor
+constructor TDevicePropertyBox.Create(AOwner: TComponent;
+  const a_Device: TAudioDevice; const a_LanguageType: TLanguageType);
+begin
+  inherited Create(AOwner);
+
+  // Store Argments
+  f_Device := a_Device;
+
+  // Set Language
+  SetLanguage(a_LanguageType);
+end;
+
+// *****************************************************************************
+// Destructor
+destructor TDevicePropertyBox.Destroy;
+begin
+  inherited;
+end;
+
+// *****************************************************************************
+// Set LanguageType
+procedure TDevicePropertyBox.SetLanguage(const a_LanguageType: TLanguageType);
+begin
+  Self.Caption := IfThen(a_LanguageType = ltEnglish, 'Device Properties',
+    'デバイス設定');
+end;
+
+// *****************************************************************************
+// Update Props
+procedure TDevicePropertyBox.UpdateProperties;
+begin;
+end;
+
+{ TPropertyBox }
+
+// *****************************************************************************
+// Constructor
+constructor TPropertyBox.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+
+  // Setting
+  Self.Align := alLeft;
+  Self.AlignWithMargins := True;
+  Self.Margins.SetBounds(PITCH_SIZE, PITCH_SIZE, PITCH_SIZE, PITCH_SIZE);
+  Self.Padding.SetBounds(PITCH_SIZE, PITCH_SIZE, PITCH_SIZE, PITCH_SIZE);
+end;
+
+// *****************************************************************************
+// Destructor
+destructor TPropertyBox.Destroy;
+begin
+  inherited;
 end;
 
 end.
